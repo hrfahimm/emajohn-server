9f9809c6451606c649f47533675d5563f991d558
hrfahimm <fahimtaj10@gmail.com>
2023-05-18 14:42:27 +0600

    'oo'

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/** @format */
+
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
@@ -23,20 +25,58 @@ async function run() {
     console.log("connected to MongoDB!");
     const database = client.db("emajohn");
     const productCollection = database.collection("products");
+    const orderCollection = database.collection("orders");
+
+    //Get Product API
+    app.get("/products", async (req, res) => {
+      console.log(req.query);
+      const cursor = productCollection.find({});
+      const page = req.query.page;
+      const size = parseInt(req.query.size);
+      let products;
+      if (page) {
+        products = await cursor
+          .skip(page * size)
+          .limit(size)
+          .toArray();
+      } else {
+        products = await cursor.toArray();
+      }
+      res.send({ count, products });
+
+      // const count = await productCollection.countDocuments({});
+    });
+    //usePost to React data ket
+    app.post("/products/bykeys", async (req, res) => {
+      const keys = req.body;
+      const query = { key: { $in: keys } };
+      const products = await productCollection.find(query).toArray();
+      res.json(products);
+    });
+    //aadd order api
+    app.post("/orders", async (req, res) => {
+      const order = req.body;
+      const result = await orderCollection.insertOne(order);
+
+      res.json(result);
+    });
+    //
   } finally {
-    await client.close();
+    //await client.close();
   }
 }
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-//   DB_USER=emajohn
-// DB_PASS=VEsChD7145e4ZrUz
   res.send("running ginius server");
 });
 
 app.listen(port, () => {
   console.log(port);
 });
-
-
+//   DB_USER=emajohn
+// DB_PASS=VEsChD7145e4ZrUz
+// git init
+// git add .
+// git commit -m 'emajohn-swrver'
+// git push
